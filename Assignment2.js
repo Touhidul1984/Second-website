2c43f9da6258ec80659ace7f8a41afe13f8ded45
Touhidul1984 <149979520+Touhidul1984@users.noreply.github.com>
2024-08-30 22:03:02 +0600

    Assignment2
    Submit Your Basic JavaScript Problem Solving

---
+++ Assignment2.js
@@ -0,0 +1,123 @@
+//1. Ans:
+function celsiusToFahrenheit(celsius) {
+    return (celsius * 9/5) + 32;
+  }
+
+  console.log(celsiusToFahrenheit(0)); // 32
+  console.log(celsiusToFahrenheit(25)); // 77
+
+  //2.Ans
+  function isEven(num) {
+    return num % 2 === 0;
+  }
+
+  console.log(isEven(4)); // true
+  console.log(isEven(7)); // false
+
+  //3. Ans:
+  function sum(a, b) {
+    return a+b;
+  }
+
+  console.log(sum(3, 4)); // 7
+  console.log(sum(10, 20)); // 30
+//4. Ans:
+function findSmallestNum(arr) {
+    return Math.min(...arr);
+  }
+
+  console.log(findSmallestNum([3, 5, 1, 9])); // 1
+  console.log(findSmallestNum([-1, -5, 0, 10])); // -5
+
+  //5.Ans:
+  function countVowels(str) {
+    let vowels = 'aeiouAEIOU';
+    let count = 0;
+     for (let char of str) {
+      if (vowels.includes(char)) {
+        count++;
+      }
+    }
+    return count;
+  }
+
+  console.log(countVowels("hello world")); // 3
+  console.log(countVowels("Javascript")); // 3
+
+  //6.Ans:
+  function getFirstElement(arr) {
+    return arr[0];
+  }
+
+  console.log(getFirstElement([1, 2, 3])); // 1
+  console.log(getFirstElement(["a", "b", "c"])); // "a"
+
+  //7. Ans:
+  function isArrayEmpty(arr) {
+    return arr.length === 0;
+  }
+  console.log(isArrayEmpty([])); // true
+  console.log(isArrayEmpty([1, 2, 3])); // false
+
+  //8.Ans:
+  function factorialize(num) {
+    if (num === 0 || num === 1) return 1;
+    return num * factorialize(num - 1);
+  }
+
+  console.log(factorialize(5)); // 120
+  console.log(factorialize(7)); // 5040
+
+  //9.Ans:
+  function reverseString(str) {
+    return str.split('').reverse().join('');
+  }
+
+  console.log(reverseString("hello")); // "olleh"
+  console.log(reverseString("world")); // "dlrow"
+
+  //10.Ans:
+  function toLowerCase(str) {
+    return str.toLowerCase();
+  }
+
+  console.log(toLowerCase("HELLO WORLD")); // "hello world"
+  console.log(toLowerCase("JavaScript")); // "javascript"
+
+  //11.Ans:
+  function stringLength(str) {
+    return str.length;
+  }
+
+  console.log(stringLength("hello")); // 5
+  console.log(stringLength("world")); // 5
+//12.Ans:
+function mergeArrays(arr1, arr2) {
+  return arr1.concat(arr2);
+}
+
+console.log(mergeArrays([1, 2, 3], [4, 5, 6])); // [1, 2, 3, 4, 5, 6]
+console.log(mergeArrays(["a", "b"], ["c", "d"])); // ["a", "b", "c", "d"]
+//13. Ans:
+function getLastElement(arr) {
+  return arr[arr.length - 1];
+}
+
+console.log(getLastElement([1, 2, 3])); // 3
+console.log(getLastElement(["a", "b", "c"])); // "c"
+//14 Ans:
+function getFirstCharacter(str) {
+  return str[0];
+}
+
+console.log(getFirstCharacter("hello")); // "h"
+console.log(getFirstCharacter("world")); // "w"
+
+//15 Ans:
+function sumArray(arr) {
+  return arr.reduce((acc, curr) => acc + curr, 0);
+}
+
+console.log(sumArray([1, 2, 3, 4])); // 10
+console.log(sumArray([-1, -2, -3, -4])); // -10
+console.log(sumArray([1.5, 2.5, 3.5])); // 7.5
